b312d53149114089f615082a31226b142649ed1c
utfw <hwan.c.0330@gmail.com>
2023-03-08 11:46:35 +0900

    upload

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import './styles/App.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Navigation />
       <Routes>
         <Route path='/' element={<Home />} />
